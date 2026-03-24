19725a25ea4cb045019eb6e39ae6b982d715c3b6
Ritesh Kushwaha <riteshkushwaha089@gmail.com>
2026-03-24 23:24:51 +0530

    05: dashboard and trpc setup

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -186,7 +186,7 @@ export const SignInView = () => {
 
           <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
             <img src="/logo.svg" alt="Image" className="h-[92px] w-[92px]" />
-            <p className="text-2xl font-semibold text-white">Call.AI</p>
+            <p className="text-2xl font-semibold text-black">Call.AI</p>
           </div>
         </CardContent>
       </Card>
